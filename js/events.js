c323bee454f580bbbdea0a9aa61d8e91e37378a1
Rose Stough <psithurise@programmer.net>
2019-04-14 20:29:54 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function pressIt() {
 
 function sumbitIt() {
  //bind submit event to form, alerts "your form is going to be submitted now."
- $('form').on("submit", function() {
+ $('form').on('submit', function() {
     alert('your form is going to be submitted now');
     return;
 })
