8172dd20665fd2424d132374d82408241c53598b
Rose Stough <psithurise@programmer.net>
2019-04-14 19:23:18 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,20 @@
 //define functions here
+function getIt() {
+ //bind click event to p tag. when clicked, alert "hey!"
+}
+
+function frameIt() {
+ //bind load event, adds class tasty to img
+}
+
+function pressIt() {
+ //bind keydown event to g key in input field of form
+}
+
+function sumbitIt() {
+ //bind submit event to form, alerts "your form is going to be submitted now."
+}
+
 
 $(document).ready(function(){
 
